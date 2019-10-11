c3bce5d22921a7551b579aae0ed8f53a37530619
tpetersfullstack <56130731+tpetersfullstack@users.noreply.github.com>
2019-10-11 10:06:26 -0600

    my push

---
+++ src/javascript/daily.js
@@ -0,0 +1,40 @@
+import { isModuleDeclaration } from "@babel/types";
+
+/*	
+	Write the function that will create this output:
+
+*** the two values are not the same:
+    p1--> a
+    p2--> b
+*** the two values are not the same:
+    p1--> 1
+    p2--> 2
+*** the two values are not the same:
+    p1--> 2
+    p2--> 2
+*/
+
+// Write the function after this comment ---
+
+const functions = {
+    add: (num1, num2) => num1 + num2
+};
+
+if(num1 !== num2) {
+
+        
+
+}
+
+export default functions;
+
+//module.exports = functions;
+
+// and before this comment ---
+
+// assertEquals("a","b");
+// assertEquals("a","a");
+// assertEquals(1,2);
+// assertEquals(2,2);
+// assertEquals("2",2);
+// assertEquals("This value","This value");
---
+++ src/javascript/daily.test.js
@@ -0,0 +1,10 @@
+import functions from './daily.js'
+
+
+//test('My Daily Test', () => {
+ //  expect(functions.size(-1)).toBe("small"); 
+ //  });
+
+ test('Todd Test to fail', () => {    
+   expect(functions.add(2,2 )).toBe(4);
+ });
