0ea0a746ececcb32c270b5e926e79b441c760cd5
tpetersfullstack <56130731+tpetersfullstack@users.noreply.github.com>
2019-10-16 11:22:06 -0600

    my push 10-16-19

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -14,28 +14,29 @@
 */
 
 // Write the function after this comment ---
-
-//Create function add
+//function? not functions? why?
+//Create an Object containing many function
 const functions = {
 
     add: (num1, num2) => num1 + num2,
 
     assertEquals: (num1, num2) => {
-        console.log("i'm in larry");
+        console.log("In assertEquals");
         return "";
     },
 
 };
-
+//This should be a inside a function
 // if (num1 !== num2) {
 //     //console.log('*** the two values are not the same:' + '\n'  + 'p1--> a' + '\n' + 'p2--> b')
 //     console.log('*** the two values are not the same:');
 //     console.log(`    p1--> ${num1}`);
 //     console.log(`    p2--> ${num2}`);
 // }
-
+//Is from the assignment or the Traversity Video?
 export default functions;
 
+//Where is this from?
 //module.exports = functions;
 
 // and before this comment ---
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -4,7 +4,7 @@ import functions from './daily.js'
 //test('My Daily Test', () => {
  //  expect(functions.size(-1)).toBe("small"); 
  //  });
-
+//This is the Stub
  test('Todd Test to fail', () => {    
    expect(functions.add(2,2 )).toBe(4);
  });
