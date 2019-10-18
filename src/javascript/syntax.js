5a361b7c59993fdeff16a8bb4c05afab715f36a5
tpetersfullstack <56130731+tpetersfullstack@users.noreply.github.com>
2019-10-18 14:28:11 -0600

    myPush 10-18-19

---
+++ src/javascript/syntax.js
@@ -0,0 +1,30 @@
+/*--------------- Copy this section into your code syntax.js as comments --------
+
+define attributes / variables
+number
+string
+boolean
+array
+dictionary / objects
+undefined
+sample if / else
+functions
+parameters
+returns
+arrays
+add to the front
+add to the end
+update values
+loops 
+for
+for/in
+while
+do while
+forEach (with array and function)
+Objects / Dictionaries
+declare object
+lookup key to retrieve value
+
+--------------- Copy ends here --------
+
+When you are done. Send Roman or Larry an email and continue to the next exercise.*/
