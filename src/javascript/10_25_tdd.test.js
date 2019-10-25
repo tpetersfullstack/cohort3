f0c4c76e3f08de6d9b600295acb95d6c4c7d399c
tpetersfullstack <56130731+tpetersfullstack@users.noreply.github.com>
2019-10-25 13:34:10 -0600

    testscript

---
+++ src/javascript/10_25_tdd.test.js
@@ -0,0 +1,15 @@
+//Write Stub (function name, parameters, and has a dummy return 0)
+//1. Add a test (tests that NPM is working)
+//2. Run all tests and see if the new test fails
+//3. Write the minimum amount of code to make test pass
+//Write another test
+//4. Run test
+//5. Refactor code
+//6. Repeat
+
+test('myTest', () => {
+    //return 0;
+    //console.log("Hello World");
+    expect (functions.sum(1,2)).toBe(3);
+});
+
