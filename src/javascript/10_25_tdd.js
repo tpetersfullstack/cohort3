55829b383d065f860db40b64b10e8f16423983b5
tpetersfullstack <56130731+tpetersfullstack@users.noreply.github.com>
2019-10-26 09:48:00 -0600

    10-26-19-1

--- src/javascript/10_25_tdd.js
+++ src/javascript/10_25_tdd.js
@@ -3,7 +3,7 @@ const functions = {
     helloWorld: (wordOne, wordTwo) => {
         console.log('Hello World from 10_25_tdd.js');
     },
-    
+
     sum: (p1, p2) => {                  //Sum function
         return p1 + p2;
     }
