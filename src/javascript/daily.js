8a7e46755e4b9d76d8cfc9387b737797aecb93b2
tpetersfullstack <56130731+tpetersfullstack@users.noreply.github.com>
2019-10-29 18:30:51 -0600

    10-29-19-4

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,84 +1,33 @@
 import { isModuleDeclaration } from "@babel/types";
-/*	
-	Write the function that will create this output:
 
-*** the two values are not the same:
-    p1--> a
-    p2--> b
-*** the two values are not the same:
-    p1--> 1
-    p2--> 2
-*** the two values are not the same:
-    p1--> 2
-    p2--> 2
-*/
-
-// Write the function after this comment ---
-
-//function? not functions? why?
-//Create an Object containing many functions(S)
+//Create an Object containing many functions
 const functions = {
 
-    add: (num1, num2) => num1 + num2,
+    //add: (num1, num2) => num1 + num2,
 
-    assertEquals: (num1, num2) => {
-        console.log("In assertEquals");
-        return "";
+    // assertEquals: (num1, num2) => {
+    //     console.log("In assertEquals");
+    //     return "";
+    // },
+
+    //Write the function makeEmailArr to build email addresses for the company.
+    makeEmailArr: (name) => {
+        const x = name[0].toLowerCase() + '.' + name[1].toLowerCase() + '@' + 'evolveu' + '.ca';
+        //return 0;
+        return x;
     },
-
 };
-//This should be a inside a function
-// if (num1 !== num2) {
-//     //console.log('*** the two values are not the same:' + '\n'  + 'p1--> a' + '\n' + 'p2--> b')
-//     console.log('*** the two values are not the same:');
-//     console.log(`    p1--> ${num1}`);
-//     console.log(`    p2--> ${num2}`);
-// }
-//Is from the assignment or the Traversity Video?
-export default functions;
-
-//Where is this from?
-//module.exports = functions;
+//let makeEmailArr = data.fname + '.' + data.lname + '@' + data.company + '.ca';
 
 // and before this comment ---
+//data.foreach(makeEmailArr);
 
-// assertEquals("a","b");
-// assertEquals("a","a");
-// assertEquals(1,2);
-// assertEquals(2,2);
-// assertEquals("2",2);
-// assertEquals("This value","This value");
+//Create the Stub to make sure the test gets here
+//Then replace with actual code
+// const functions = {
+//     emailBuilder: (fname, lname) => {
+//         return ('l.shumlich@evolveu.ca');
+//     },
+// };
 
-/*
-    Sample data for the next few exercises.
-*/
-
-const data = {
-    staff: [
-        { fname: "Jane", lname: "Smith", balance: 10 },
-        { fname: "Liam", lname: "Henry", balance: 1000 },
-        { fname: "Emma", lname: "Jones", balance: 1330 },
-        { fname: "Olivia", lname: "Notly", balance: 310 },
-        { fname: "Noah", lname: "Ho", balance: 503 },
-        { fname: "William", lname: "Lee", balance: 520 },
-        { fname: "Benjamin", lname: "Amis", balance: 150 },
-    ],
-    company: "EvolveU",
-    city: "Calgary",
-    prov: "Alberta"
-};
-
-
-/*	
-	Write the function to build email addresses for the company.
-*/
-
-test('email builder for company', () => {
-    const staffEmail = functions.loopStaff(data.staff);
-    expect(staffEmail[0])
-        .toEqual("jane.smith@evolveu.ca");
-    expect(staffEmail[3])
-        .toEqual("olivia.notly@evolveu.ca");
-    expect(staffEmail[6])
-        .toEqual("benjamin.amis@evolveu.ca");
-});
+export default functions;
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -2,13 +2,34 @@
 /* Jest is a "Test Runner" and an "Assertion Library" containing Methods to test your code */
 /* *************************************************************************************** */
 import functions from './daily.js'    //Gets/Tells jest where to look for your Stubs
-//Asertion - EXPECT to be true
-/* ******************************** Unit Test 1 ****************************************** */
-test('myTest1', () => {               //test is a jest function w/methods
-  expect(functions.add(2, 2)).toBe(4);
-});
+//Assertion - EXPECTED to be true
+// /* ******************************** Unit Test 1 ****************************************** */
+// test('myTest1', () => {               //test is a jest function w/methods
+//   expect(functions.add(2, 2)).toBe(4);
+// });
 
-/* ******************************** Unit Test 2 ****************************************** */
-test('myTest2', () => {
-  expect(functions.assertEquals(2, 2)).toBe(4); //These are assertions
+// /* ******************************** Unit Test 2 ****************************************** */
+// test('myTest2', () => {
+//   expect(functions.assertEquals(2, 2)).toBe(4); //These are assertions
+// });
+
+// test('emailBuilderTest', () => {
+//   expect(functions.emailBuilder('larry', 'shumlich')).toEqual('l.shumlich@evolveu.ca');
+// }); 
+
+/*
+    Write a function to format an email based on an array.
+*/
+test('email builder from an array', () => {
+
+    const name = ["first", "last"];
+
+    expect(functions.makeEmailArr(name))
+        .toEqual("first.last@evolveu.ca");
+
+    expect(functions.makeEmailArr(["First", "Last"]))
+        .toEqual("first.last@evolveu.ca");
+
+    expect(functions.makeEmailArr(["Bill", "Smith"]))
+        .toEqual("bill.smith@evolveu.ca");
 });
