e78385dbc346d1853019a84f2d09929fad3dc0d0
tpetersfullstack <56130731+tpetersfullstack@users.noreply.github.com>
2019-11-10 13:22:42 -0700

    11-10-19-3

--- src/01-getting-started/scripts/functions.js
+++ src/01-getting-started/scripts/functions.js
@@ -1,9 +1,4 @@
-
 const functions = {
-
-    helloWorld: (wordOne, wordTwo) => {
-        console.log('Hello World from 10_25_tdd.js');
-    },
 
     sum: (p1, p2) => {                  //Sum function
         return p1 + p2;
---
+++ src/01-getting-started/scripts/functions.test.js
@@ -0,0 +1,27 @@
+import functions from './functions'
+
+// test('Check the sizes', () => {
+//     expect(functions.size(-1)).toBe("small"); // Consider the edge cases
+//     expect(functions.size(0)).toBe("small");
+//     expect(functions.size(10)).toBe("medium");
+//     expect(functions.size(15)).toBe("medium");
+//     expect(functions.size(20)).toBe("large");
+//     expect(functions.size(2000000)).toBe("large");
+// });
+
+test('Sum', () => {
+    expect(functions.add(1,2)).toBe(3);
+    expect(functions.add(101,202)).toBe(303);
+});
+test('Subtract', () => {
+    expect(functions.sub(10,2)).toBe(8);
+    expect(functions.sub(202,102)).toBe(100);
+});
+test('Multiply', () => {
+    expect(functions.mult(5,5)).toBe(25);
+    expect(functions.mult(101,202)).toBe(303);
+});
+test('Division', () => {
+    expect(functions.div(25,5)).toBe(5);
+    expect(functions.div(100,5)).toBe(20);
+});
