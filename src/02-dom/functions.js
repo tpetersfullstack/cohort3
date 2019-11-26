046ea6e9e41169e849c4f2e91967a7f26b851ed4
tpetersfullstack <56130731+tpetersfullstack@users.noreply.github.com>
2019-11-25 19:14:09 -0700

    11-25-19-2

--- src/02-dom/functions.js
+++ src/02-dom/functions.js
@@ -1,18 +1,45 @@
 /* jshint undef: true, unused: true */
 /* globals MY_GLOBAL */
-
+/*
+Declare Global Variables Here - to Keep track of State, Counts etc.
+*/
+var cardCount;
 const functions = {
 
-   myAddFunction: () => {
-        var node = document.createElement("LI");                        //First create an LI node
-        var textnode = document.createTextNode("Water");                //then create a Text node
-        node.appendChild(textnode);                                     //then append the Text node to the LI node
-        document.getElementById("idOrderedList").appendChild(node);     //Finally append the LI node to the list
-      },
-      showButtonFunction: () => {
-          console.log(idOrderedList.childNodes);
-      },
+  myAddFunction: () => {
+    var node = document.createElement("LI"); //First create an LI node
+    var textnode = document.createTextNode("Water"); //then create a Text node
+    node.appendChild(textnode); //then append the Text node to the LI node
+    document.getElementById("idOrderedList").appendChild(node); //Finally append the LI node to the list
+  },
+  showButtonFunction: () => {
+    console.log(idOrderedList.childNodes);
+  },
+/* 
+Working with Cards functions
+*/
+/*
+Create Card Function
+*/
+addCardFunction: () => { 
+  // create a new Card(div element) This should be an array object
+  // or store this as a card count
+  //This can only be used once?
+  var newDiv = document.createElement("div"); 
+  // and give it some content 
+  var newContent = document.createTextNode("Card "); 
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+  // add the newly created element and its content into the DOM 
+  var currentDiv = document.getElementById("div1"); 
+  // Insert the new div before(?) pass the newly created div
+  // and the current div(parent) 
+  document.body.insertBefore(newDiv, currentDiv); 
+},
+};          //Close addCardFunction
 
-};
+/*
+Delete Card Function
+*/
 
 export default functions;
--- src/02-dom/main.js
+++ src/02-dom/main.js
@@ -1,5 +1,16 @@
 /* jshint undef: true, unused: true */
 /* globals MY_GLOBAL */
+/*
+Working With Cards Assignment: Todd Peters 11-24-19
+
+Create a New Section, then create 2 panels - a left and a right. 
+The left side has card-like images inside with three buttons on each card
+one Add card button on the left main div
+on ech card there is a counter, with a card label:
+as well each card will have an add before, add after and a delete button
+the right side is to remain blank for now
+use css classes
+*/
 
 import functions from "./functions.js";
 
@@ -18,8 +29,30 @@ idParent.addEventListener('click', () => {
     console.log('In Parent div Event Listener');
     console.log(event.target);
 });
-
+//show?
 idOrderedList.addEventListener("show", () => {
     console.log("In OL Event Listener");
     console.log(event.target);
 });
+/*
+Working with Cards
+*/
+idButtonAddCard.addEventListener("click", () => {
+    console.log("Add Card Event Listener");
+    console.log(event.target);
+});
+
+// idOrderedList.addEventListener("show", () => {
+//     console.log("In OL Event Listener");
+//     console.log(event.target);
+// });
+
+// idOrderedList.addEventListener("show", () => {
+//     console.log("In OL Event Listener");
+//     console.log(event.target);
+// });
+
+// idOrderedList.addEventListener("show", () => {
+//     console.log("In OL Event Listener");
+//     console.log(event.target);
+// });
