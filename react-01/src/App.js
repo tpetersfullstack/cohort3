3c1f0c3395cc577b63be7c8d18f5254e2de52546
tpetersfullstack <56130731+tpetersfullstack@users.noreply.github.com>
2019-12-14 09:41:03 -0700

    12-14-19-2

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,27 +1,52 @@
 import React from 'react';
+//import MyComp from './components/MyComponent';
+import {MyComp, EvenComp, OddComp} from './components/MyComponent';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>I am in control of this application and my name is Todd</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.counter = 21;
+    this.state = {
+      myState: "TBD"
+    };
+  }
 
+  onPushMe = () => {
+    console.log("You pushed me");
+    this.counter += 1;
+    console.log(this.counter);
+    this.setState({
+      myState: "now:" + this.counter
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>I am in control of this application and my name is Todd {this.counter} {this.state.myState}</h1>
+          <button onClick={this.onPushMe}>
+            Push Me
+          </button>
+          <MyComp whatToSay={"What Ever"}
+            onPushMeOutput={this.onPushMe} />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+        </a>
+        </header>
+      </div>
+    );
+  }
+}
 export default App;
